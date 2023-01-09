412ca7ef80d4b8505ba6de7f674cd514e52b4599
Samriddha <samriddha97@gmail.com>
2023-01-09 20:51:22 +0530

    before and after annotation

--- app/src/test/java/com/example/androidtestdemo/helpers/HelperTest.kt
+++ app/src/test/java/com/example/androidtestdemo/helpers/HelperTest.kt
@@ -1,10 +1,33 @@
 package com.example.androidtestdemo.helpers
 
+import org.junit.After
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
 
 
 class HelperTest {
+
+    private lateinit var helper: Helper
+
+    /*
+    * This function will execute before every test case.
+    * As we have annotated with @Before
+    * */
+    @Before
+    fun beforeTestCase(){
+        println("Execute before every test case.")
+        helper = Helper()
+    }
+
+    /*
+    * This function will execute after every test case.
+    * As we have annotated with @After
+    * */
+    @After
+    fun afterTestCase(){
+        println("Execute after every test case.")
+    }
 
     @Test
     fun isPalindrome_inputString_palindrome_expectedFalse() {
@@ -18,8 +41,6 @@ class HelperTest {
 
     @Test
     fun isPalindrome_inputString_level_expectedTrue() {
-        // Arrange
-        val helper = Helper()
         // Act
         val result = helper.isPalindrome("Level")
         // Assert
